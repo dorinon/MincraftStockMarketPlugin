5bb3c70ebde3f9411e5036324f77b6a75883d04c
Mai Porat <63251610+MaiTheLord@users.noreply.github.com>
2022-07-28 11:07:05 +0300

    file thingy

--- src/main/java/com/dorinon/market/Database.java
+++ src/main/java/com/dorinon/market/Database.java
@@ -2,6 +2,7 @@ package com.dorinon.market;
 
 import org.bukkit.plugin.Plugin;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,7 +12,9 @@ public final class Database {
     private final Connection connection;
 
     Database(Plugin plugin) throws SQLException {
-        connection = DriverManager.getConnection("jdbc:sqlite:%s".formatted(plugin.getDataFolder().toPath().resolve("database.db")));
+        File databaseFile = plugin.getDataFolder().toPath().resolve("database.db").toFile();
+
+        connection = DriverManager.getConnection("jdbc:sqlite:%s".formatted(databaseFile.getAbsolutePath()));
     }
 
     public void getItemPrice() {
