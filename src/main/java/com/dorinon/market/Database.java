e249eccc48374ae181157911a8186bc0087a0388
Mai Porat <63251610+MaiTheLord@users.noreply.github.com>
2022-07-28 18:14:22 +0300

    schema thing hduiathaio gaga

--- src/main/java/com/dorinon/market/Database.java
+++ src/main/java/com/dorinon/market/Database.java
@@ -4,25 +4,40 @@ import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
+import java.util.List;
 
 public final class Database {
     private final static String DATABASE_FILE_NAME = "database.db";
-    private final static int DATABASE_SCHEMA_VERSION = 1;
+
+    private final static List<UpgradeHandler> SCHEME_UPGRADE_HANDLERS = List.of(
+            connection -> connection.createStatement().execute("""
+                    CREATE TABLE offers
+                    (
+                        player_uuid TEXT NOT NULL,
+                        item_id     TEXT NOT NULL,
+                        amount      INT DEFAULT 1,
+                        offer_type  INT  NOT NULL,
+                        cost        REAL NOT NULL
+                    )
+            """)
+    );
 
     private final Connection connection;
 
     public Database(@NotNull Plugin plugin) throws SQLException {
         plugin.getDataFolder().mkdir();
-
-        connection = DriverManager.getConnection(
-                "jdbc:sqlite:%s".formatted(plugin.getDataFolder().toPath().resolve(DATABASE_FILE_NAME))
-        );
-
-        handleSchema();
+        connection = DriverManager.getConnection("jdbc:sqlite:%s".formatted(plugin.getDataFolder().toPath().resolve(DATABASE_FILE_NAME)));
+        handleSchema(connection);
     }
 
-    private void handleSchema() throws SQLException {
-        ResultSet rs = connection.createStatement().executeQuery("PRAGMA USER_VERSION");
-        System.out.println(rs);
+    private void handleSchema(@NotNull Connection connection) throws SQLException {
+        int currentSchema = connection.createStatement().executeQuery("PRAGMA user_version").getInt(1);
+        for (int i = currentSchema; i < SCHEME_UPGRADE_HANDLERS.size(); i++) SCHEME_UPGRADE_HANDLERS.get(i).accept(connection);
+        connection.createStatement().execute("PRAGMA user_version = %d".formatted(SCHEME_UPGRADE_HANDLERS.size()));
+    }
+
+    @FunctionalInterface
+    private interface UpgradeHandler {
+        void accept(Connection connection) throws SQLException;
     }
 }
